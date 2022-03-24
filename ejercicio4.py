e77c0dbf7b311d63c30af2f5a9f88277b0847963
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-24 08:59:20 +0100

    desarrollo la función para crear ya la cuenta bancaria y una de ellas será por defecto

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,5 @@
+import random
+from datetime import datetime
 class Cuenta():
     def __init__(self, idaccount, nombre, apertura, numero, saldo, modo): # Desarrollo el constructor
         self.id = idaccount
@@ -37,8 +39,18 @@ class Cuenta():
         print("Usted ha transferido " + str(dinerotransferencia) + "€ y su saldo actualmente es de " + str(self.saldo) + "€.")
         print("El saldo de la cuenta B es de " + str(B.saldo) + "€.")
             
+def creacioncuenta():
+            nombre = str(input("""Ha seleccionado usted la opción de crear una cuenta bancaria.
 
+Introduzca su nombre por favor: 
 
-A = Cuenta(21323434, "Juan", "2 de enero", 23294934, 10000, 1)
-B = Cuenta(21323432, "Ruben", "5 de enero", 43294934, 20000, 1)
-Cuenta.transferencia(A)
+"""))
+            global A, B
+            modo = int(input("Si desea crear una cuenta a plazo fijo pulse 1, en cambio si desea crear una cuenta VIP pulse cualquier otro valor: "))
+            numero = str(random.randint(100000000000, 999999999999))
+            A = Cuenta(str(random.randint(100000, 999999)), nombre, datetime.today(),numero, 10000, modo)
+            print("Hola " + nombre + ". Usted ha creado una cuenta en nuestro banco a fecha de " + str(datetime.today()) + " con el número de cuenta: " + numero)
+            B = Cuenta(str(random.randint(100000, 999999)), "Persona B", datetime.today(), str(random.randint(100000000000, 999999999999)), 10000, 5)
+            print("La segunda cuenta con la que va a poder operar ha sido creada por defecto a nombre de Esther, con un saldo también de 10000 euros y fue creada el 15/07/2014, es una cuenta VIP.")
+
+creacioncuenta()
