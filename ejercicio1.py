5b37fddcf2f41dc61a9613d07b1905a98bbfcfb8
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-24 08:40:12 +0100

    añado variables en el constructor

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,8 @@
 # Ejercicio 1
 class Libro():
-    def __init__(self, titulo, npaginas, fechapublicacion):
+    def __init__(self, titulo, npaginas, fechapublicacion, autor, sbn):
         self.titulo = titulo
         self.npaginas = npaginas
         self.fechapublicacion = fechapublicacion
+        self.autor = autor
+        self.sbn = sbn
