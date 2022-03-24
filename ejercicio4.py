d014824b3f1fc9f218cdeee37762734cbb8ac6fc
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-24 08:42:31 +0100

    desarrollo el constructor

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,2 +1,8 @@
 class Cuenta():
-    
+    def __init__(self, idaccount, nombre, apertura, numero, saldo, modo): # Desarrollo el constructor
+        self.id = idaccount
+        self.nombre = nombre
+        self.apertura = apertura
+        self.numero = numero
+        self.saldo = saldo
+        self.modo = modo
